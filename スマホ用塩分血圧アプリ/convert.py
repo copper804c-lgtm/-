311959ff97dfd84c5af55e8d1cde77a5fcf9da23
copper804c-lgtm <copper804.c@gmail.com>
2026-03-14 00:03:58 +0900

    Initial commit
    配布

---
+++ スマホ用塩分血圧アプリ/convert.py
@@ -0,0 +1,8 @@
+from defusedxml import ElementTree as etree
+import cairosvg
+import os
+
+svg_path = 'icon.svg'
+cairosvg.svg2png(url=svg_path, write_to='icon-192.png', output_width=192, output_height=192)
+cairosvg.svg2png(url=svg_path, write_to='icon-512.png', output_width=512, output_height=512)
+print('Conversion successful')
